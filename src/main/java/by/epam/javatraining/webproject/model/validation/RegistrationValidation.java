21dcd4184d1c8d95537a33be775d356af5fed9d4
Kate <kayara998@gmail.com>
2019-05-16 01:43:53 +0300

    updated login validation

--- src/main/java/by/epam/javatraining/webproject/model/validation/RegistrationValidation.java
+++ src/main/java/by/epam/javatraining/webproject/model/validation/RegistrationValidation.java
@@ -1,5 +1,8 @@
 package by.epam.javatraining.webproject.model.validation;
 
+import by.epam.javatraining.webproject.model.entity.User;
+import by.epam.javatraining.webproject.model.service.UserService;
+import by.epam.javatraining.webproject.model.service.exception.UserServiceException;
 import org.apache.log4j.Logger;
 
 public class RegistrationValidation {
@@ -11,8 +14,8 @@ public class RegistrationValidation {
     }
 
 
-    public static final String NAME_PATTERN = "[А-Яа-я]{1,20}";
-    public static final String LOGIN_PATTERN = "^(?=.+\\@\\w{4,6}\\.\\w{2,3}).{10,25}$";
+    public static final String NAME_PATTERN = "[А-ЯA-Z][А-Яа-яA-Za-z]{1,20}";
+    public static final String LOGIN_PATTERN = "^(?=.+\\@\\w{4,6}\\.\\w{2,3}).{10,20}$";
     public static final String PASSWORD_PATTERN = "^(?=.*[0-9].*[0-9]).*.{6,10}$";
 
     public static boolean validateNameComponent(String string){
@@ -21,6 +24,20 @@ public class RegistrationValidation {
 
     public static boolean validateLogin(String string){
         return string.matches(LOGIN_PATTERN);
+    }
+
+    public static boolean checkLoginUniqueness(User sessionUser, UserService userService, String login) {
+        boolean result = false;
+        try {
+            User user = userService.login(login);
+            if (user != null && user.equals(sessionUser)){
+                logger.debug("comparing users: "  + user + " " + sessionUser);
+                result = true;
+            }
+        } catch (UserServiceException e) {
+            logger.error(e.getMessage());
+        }
+        return result;
     }
 
     public static boolean validatePassword(String string){
